3e2b7b23e35e9a58c8dbb5c1f6d9d698fe1ee0b7
Robertcdelfin <42871949+Robertcdelfin@users.noreply.github.com>
2019-02-14 15:39:20 -0800

    Add files via upload

---
+++ CS2650_Hw2_PatternDisplay.c
@@ -0,0 +1,29 @@
+//Robert Delfin
+//CS2650
+#include <stdio.h>
+
+int main(int argc, char **argv){
+
+	//declare row for pattern a
+	printf("Pattern A\n");
+
+	//Number of rows
+	for(int aRows = 0; aRows < 10; aRows++){
+		//Number of Colums
+		for(int aColums = 0 ; aColums < aRows; aColums++ ){
+			printf("+");
+		}
+		printf("\n");
+	}
+	printf("Pattern B\n");
+
+	for(int bRows = 10 ; bRows > 0; bRows-- ){
+
+		for(int bColums = 0; bColums < bRows ; bColums++){
+			printf("+");
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
